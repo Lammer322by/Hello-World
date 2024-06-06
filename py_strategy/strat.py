b5bc2256a6599f271fe52155ac9492e20cec05fe
Lammer_322_by <53752618+Lammer322by@users.noreply.github.com>
2024-06-06 23:23:46 +0300

    Add files via upload

---
+++ py_strategy/strat.py
@@ -0,0 +1,51 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__, static_folder="static", static_url_path="", template_folder="templates")
+
+class strategy():
+    def __init__(self,handler,request):
+        self.handler=handler
+        self.request=request
+
+    def Handle(self,greets):
+        self.handler.handle(greets,self.request)
+
+class GetHandler:
+    def handle(self,greets,request):
+        return request.form.get(greets)        
+        
+class PostHandler:
+    def handle(self,greets,request):
+        return request.form.get(greets)       
+      
+@app.route('/')
+def root():
+    return render_template(
+        'index.html'
+    )
+
+@app.route('/home/cirno_game', methods = ['GET', 'POST'])
+def hello_name():
+    if request.method == 'GET':
+        form=strategy(GetHandler,request)
+    elif request.method == 'POST':
+        form=strategy(PostHandler,request)
+
+    word_in=form.Handle('Enter your word')
+
+    if word_in is None:
+        word_in=""
+    
+    if word_in == 'your word':
+        reaction = "Congrats!"
+    else:
+        reaction = "Please, try again"
+
+    return render_template(
+        'base.html',
+        word=word_in,
+        reaction=reaction
+    )
+
+if __name__ == '__main__':
+   app.run(debug = True)
